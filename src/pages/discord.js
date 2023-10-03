3986b6a17f53a32cc26599ed2d8c95c6e6406ea2
Funasitien <bwha.bwha.splash@gmail.com>
2023-10-03 20:56:06 +0200

    🐛 Try to fix redirect bug #1

--- src/pages/discord.js
+++ src/pages/discord.js
@@ -1,15 +1,10 @@
 import React from 'react';
-import clsx from 'clsx';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
 
-import styles from './index.module.css';
 
 export default function Home() {
-  const {siteConfig} = useDocusaurusContext();
   return (
-    window.location.replace('https://codefrontend.com')
+    <BrowserOnly>
+      {() => window.location.replace('https://codefrontend.com')}
+    </BrowserOnly>
   );
 }
