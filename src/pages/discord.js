f30d778db41a5495f404b55f35ea2bc40948e1e7
Funasitien <bwha.bwha.splash@gmail.com>
2023-10-03 20:48:57 +0200

    🚧 Discord Redirect WIP

---
+++ src/pages/discord.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import clsx from 'clsx';
+import Link from '@docusaurus/Link';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import Layout from '@theme/Layout';
+import HomepageFeatures from '@site/src/components/HomepageFeatures';
+
+import styles from './index.module.css';
+
+export default function Home() {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    window.location.replace('https://codefrontend.com')
+  );
+}
