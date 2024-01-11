e061adcad93eb79b8cb48b1988361ab88547059f
Funasitien DreamCloud <86372093+Funasitien@users.noreply.github.com>
2024-01-11 08:24:05 +0100

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,8 +30,8 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`⭐ Bienvenu sur ${siteConfig.title} !`}
+      description="Découvrez Kernacraft, le serveur officiel de Kernanec, et sa magnifique documentation qui est juste trop swag.">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
